ee2ee785eec3568db522a9155487e63bbe660512
minhduc8a2 <minhduc8a2.1@gmail.com>
2024-03-29 08:22:34 +0700

    add create contact

--- src/router/index.js
+++ src/router/index.js
@@ -15,9 +15,14 @@ const routes = [
     path: "/contacts/:id",
     name: "contact.edit",
     component: () => import("@/views/ContactEdit.vue"),
-    props: true // Truyền các biến trong $route.params vào làm props
-    },
-    
+    props: true, // Truyền các biến trong $route.params vào làm props
+  },
+  {
+    path: "/contacts/create",
+    name: "contact.add",
+    component: () => import("@/views/ContactCreate.vue"),
+    props: true, // Truyền các biến trong $route.params vào làm props
+  },
 ];
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
